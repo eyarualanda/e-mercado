3129b43e934651bcc3af6bcde4444b23dff15362
abichuelah <jimenamarquez4@gmail.com>
2024-08-22 10:35:15 -0300

    Cambios login js y desafiate

---
+++ js/login.js
@@ -0,0 +1,33 @@
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    // Obtener valores de los campos de entrada
+    var usuario = document.getElementById('inputUsuario').value;
+    var contrasenia = document.getElementById('inputContraseña').value;
+
+    // Verificará si los campos están vacíos
+    if (usuario === "" || contrasenia === "") {
+        // Previene el envío del formulario con campos vacíos
+        event.preventDefault();
+        alert("Por favor, completa ambos campos.");
+    }else{
+        // Redirige a la página de portada si los campos no están vacíos
+        event.preventDefault();  // Previene la recarga del formulario
+        location.href = "index.html";  // Cambia "index.html" a la ruta correcta
+                // Guarda la sesión en localStorage
+localStorage.setItem('sesionIniciada', 'true');
+  location.href = "index.html";  // Redirige a la página de portada
+}
+});
+
+window.onload = function () {
+    // Verifica si la sesión no está iniciada
+if (localStorage.getItem('sesionIniciada') !== 'true') {
+        location.href = 'login.html';  // Redirige al login
+}
+};
+document.getElementById('cerrarSesion').addEventListener('click', function () {
+              // Elimina la sesión de localStorage
+            localStorage.removeItem('sesionIniciada')
+              // Redirige al login
+            location.href = 'login.html';
+        ;
+    })
