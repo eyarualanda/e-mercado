7f2086723ec43c9e1091fed6b79f49f6e22bb544
Sofia <sofia.stazi04@gmail.com>
2024-09-26 12:56:10 -0300

    cosas

--- js/product-info.js
+++ js/product-info.js
@@ -8,6 +8,7 @@ let opiniones = [];
 
 function mostrarInfoProducto(product) {
     infoProducto.innerHTML = '';
+    divProductosRelacionados.innerHTML = '';
 
     const carouselIndicators = product.images.map((_, index) => `
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="${index}" class="${index === 0 ? 'active' : ''}" aria-current="${index === 0 ? 'true' : ''}" aria-label="Slide ${index + 1}"></button>
@@ -20,55 +21,58 @@ function mostrarInfoProducto(product) {
     `).join('');
 
     infoProducto.innerHTML = `
-    <p class="text-muted">
-        <a href="categories.html" class="text-decoration-none text-reset">Categorías</a>
-        >
-        <a href="products.html" class="text-decoration-none text-reset">${product.category}</a>
-    </p>
+        <p class="text-muted">
+            <a href="categories.html" class="text-decoration-none text-reset">Categorías</a>
+            >
+            <a href="products.html" class="text-decoration-none text-reset">${product.category}</a>
+        </p>
 
-    <div class="col-md-7">
-        <div id="carouselExampleIndicators" class="carousel slide">
-            <div class="carousel-indicators">
-            ${carouselIndicators}
+        <div class="col-md-7">
+            <div id="carouselExampleIndicators" class="carousel slide">
+                <div class="carousel-indicators">
+                    ${carouselIndicators}
+                </div>
+                <div class="carousel-inner">
+                    ${carouselItems}
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
             </div>
-            <div class="carousel-inner">
-            ${carouselItems}
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-            </button>
         </div>
-    </div>
-    
-    <div class="col-md-5 my-3">
-        <h2 class="fw-bold">${product.name}</h2>
-        <hr class="my-4">
-        <h5 class="fw-bold"> ${product.currency} ${product.cost}</h5>
-        <p class="small text-muted">Cantidad vendida: ${product.soldCount}</p>
-        <div class="col-md-3 my-3">
+        
+        <div class="col-md-5 my-3">
+            <h2 class="fw-bold">${product.name}</h2>
+            <hr class="my-4">
+            <h5 class="fw-bold"> ${product.currency} ${product.cost}</h5>
+            <p class="small text-muted">Cantidad vendida: ${product.soldCount}</p>
+            <div class="col-md-3 my-3">
                 <label for="quantity" class="form-label">Cantidad:</label>
                 <input id="quantity" type="number" name="quantity" min="1" value="1" class="form-control">
-        </div> 
-        <div class="d-grid gap-2">
-            <button id="buyButton" class="btn btn-primary w-10 p-2">
-                <i class="fas fa-shopping-bag"></i> Comprar
-            </button>
-            <button id="addToCartButton" class="btn btn-secondary w-10 p-2">
-                <i class="fas fa-cart-plus"></i> Agregar al carrito
-            </button>
+            </div> 
+            <div class="d-grid gap-2">
+                <button id="buyButton" class="btn btn-primary w-10 p-2">
+                    <i class="fas fa-shopping-bag"></i> Comprar
+                </button>
+                <button id="addToCartButton" class="btn btn-secondary w-10 p-2">
+                    <i class="fas fa-cart-plus"></i> Agregar al carrito
+                </button>
+            </div>
         </div>
-    </div>
-    <div class="col my-4">
-        <h5 class="fw-bold">Características: </h5>
-        <hr>
-        <p>${product.description}</p>
-    </div>
+        <div class="col my-4">
+            <h5 class="fw-bold">Características: </h5>
+            <hr>
+            <p>${product.description}</p>
+        </div>
     `;
+
+    // Crear tarjetas para los productos relacionados: mostrarProductosRelacionados(`${product.relatedProducts}`);
+    
 }
 
 function mostrarComentariosProducto(opiniones) {
@@ -135,6 +139,31 @@ function mostrarComentariosProducto(opiniones) {
     generarEstrellas();
     document.getElementById('enviarReseniaButton').addEventListener('click', enviarResenia);
 }
+
+// function mostrarProductosRelacionados(productosRelacionados) {
+//     if (productosRelacionados.length > 0) {
+//         const tarjetasHTML = productosRelacionados.map(producto => `
+//             <div class="col-md-4 mb-4">
+//                 <div class="card">
+//                     <img src="${producto.image}" class="card-img-top" alt="${producto.name}">
+//                     <div class="card-body">
+//                         <h5 class="card-title">${producto.name}</h5>
+//                         <p class="card-text">${producto.currency} ${producto.cost}</p>
+//                         <a href="product.html?productID=${producto.id}" class="btn btn-primary">Ver producto</a>
+//                     </div>
+//                 </div>
+//             </div>
+//         `).join('');
+
+//         divProductosRelacionados.innerHTML = `
+//             <h5 class="fw-bold">Productos relacionados</h5>
+//             <hr>
+//             <div class="row">
+//                 ${tarjetasHTML}
+//             </div>
+//         `;
+//     }
+// }
 
 function formatearFecha(fechaISO) {
     const fecha = new Date(fechaISO); // Convertimos la cadena de fecha (ejemplo: "2024-09-26") en una fecha que JavaScript pueda entender
